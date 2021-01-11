fa66dbcd0101a98c90c6b84a19f88f53ac21c3e5
Tom Decroos <tomdecroos@hotmail.com>
2021-01-11 10:56:47 +0000

    Trying to fix readthedocs autodoc
    Tr

--- docs/conf.py
+++ docs/conf.py
@@ -66,3 +66,14 @@ html_theme_options = {
 html_static_path = ['_static']
 html_css_files = ["custom.css"]
 
+# mock imported packages
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'pandas','xgboost']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
