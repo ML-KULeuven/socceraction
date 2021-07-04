3839efe1493a630dac2b10cde180975cafa48185
Jan Van Haaren <janvanhaaren@gmail.com>
2021-07-04 12:43:41 +0200

    Add MA1 JSON feed parser

--- socceraction/spadl/opta.py
+++ socceraction/spadl/opta.py
@@ -749,6 +749,66 @@ class _F24JSONParser(OptaJSONParser):
         return events
 
 
+class _MA1JSONParser(OptaJSONParser):
+    def get_match_info(self) -> Dict[str, Any]:
+        if 'matchInfo' in self.root:
+            return self.root['matchInfo']
+        raise MissingDataError
+
+    def get_live_data(self) -> Dict[str, Any]:
+        if 'liveData' in self.root:
+            return self.root['liveData']
+        raise MissingDataError
+
+    def extract_competitions(self) -> Dict[int, Dict[str, Any]]:
+        match_info = self.get_match_info()
+        tournament_calender = assertget(match_info, 'tournamentCalendar')
+        competition = assertget(match_info, 'competition')
+        season_id = assertget(tournament_calender, 'id')
+        season = dict(
+            season_id=assertget(tournament_calender, 'id'),
+            season_name=assertget(tournament_calender, 'name'),
+            competition_id=assertget(competition, 'id'),
+            competition_name=assertget(competition, 'name'),
+        )
+        return {season_id: season}
+
+    def extract_teams(self) -> Dict[int, Dict[str, Any]]:
+        match_info = self.get_match_info()
+        contestants = assertget(match_info, 'contestant')
+        teams = {}
+        for contestant in contestants:
+            team_id = assertget(contestant, 'id')
+            team = dict(
+                team_id=team_id,
+                team_name=assertget(contestant, 'name'),
+            )
+            teams[team_id] = team
+        return teams
+
+    def extract_players(self) -> Dict[int, Dict[str, Any]]:
+        live_data = self.get_live_data()
+        lineups = assertget(live_data, 'lineUp')
+        players = {}
+        for lineup in lineups:
+            team_id = assertget(lineup, 'contestantId')
+            players_in_lineup = assertget(lineup, 'player')
+            for individual in players_in_lineup:
+                player_id = assertget(individual, 'playerId')
+                player = dict(
+                    team_id=team_id,
+                    player_id=player_id,
+                    firstname=assertget(individual, 'firstName').strip() or None,
+                    lastname=assertget(individual, 'lastName').strip() or None,
+                    nickname=assertget(individual, 'matchName').strip() or None,
+                )
+                for name_field in ['firstname', 'lastname', 'nickname']:
+                    if player[name_field]:
+                        player[name_field] = unidecode.unidecode(player[name_field])
+                players[player_id] = player
+        return players
+
+
 class _F7XMLParser(OptaXMLParser):
     def get_doc(self) -> Type[objectify.ObjectifiedElement]:
         optadocument = self.root.find('SoccerDocument')
