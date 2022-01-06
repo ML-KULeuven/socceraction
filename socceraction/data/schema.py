b207b1a365d1e461a4c9363fe2b1c4d3b5b5fd28
Pieter Robberechts <pieter_robberechts@msn.com>
2022-01-06 16:46:24 +0100

    Document the StatsBomb data schemas

--- socceraction/data/schema.py
+++ socceraction/data/schema.py
@@ -13,9 +13,13 @@ class CompetitionSchema(pa.SchemaModel):
     """Definition of a dataframe containing a list of competitions and seasons."""
 
     season_id: Series[Object] = pa.Field()
+    """The unique identifier for the season."""
     season_name: Series[str] = pa.Field()
+    """The name of the season."""
     competition_id: Series[Object] = pa.Field()
+    """The unique identifier for the competition."""
     competition_name: Series[str] = pa.Field()
+    """The name of the competition."""
 
     class Config:  # noqa: D106
         strict = True
@@ -26,12 +30,19 @@ class GameSchema(pa.SchemaModel):
     """Definition of a dataframe containing a list of games."""
 
     game_id: Series[Object] = pa.Field()
+    """The unique identifier for the game."""
     season_id: Series[Object] = pa.Field()
+    """The unique identifier for the season."""
     competition_id: Series[Object] = pa.Field()
+    """The unique identifier for the competition."""
     game_day: Series[int] = pa.Field()
+    """Number corresponding to the weeks or rounds into the competition this game is."""
     game_date: Series[DateTime] = pa.Field()
+    """The date when the game was played."""
     home_team_id: Series[Object] = pa.Field()
+    """The unique identifier for the home team in this game."""
     away_team_id: Series[Object] = pa.Field()
+    """The unique identifier for the away team in this game."""
 
     class Config:  # noqa: D106
         strict = True
@@ -42,7 +53,9 @@ class TeamSchema(pa.SchemaModel):
     """Definition of a dataframe containing the list of teams of a game."""
 
     team_id: Series[Object] = pa.Field()
+    """The unique identifier for the team."""
     team_name: Series[str] = pa.Field()
+    """The name of the team."""
 
     class Config:  # noqa: D106
         strict = True
@@ -50,15 +63,22 @@ class TeamSchema(pa.SchemaModel):
 
 
 class PlayerSchema(pa.SchemaModel):
-    """Definition of a dataframe containing the list of players of a game."""
+    """Definition of a dataframe containing the list of players on the teamsheet of a game."""
 
     game_id: Series[Object] = pa.Field()
+    """The unique identifier for the game."""
     team_id: Series[Object] = pa.Field()
+    """The unique identifier for the player's team."""
     player_id: Series[Object] = pa.Field()
+    """The unique identifier for the player."""
     player_name: Series[str] = pa.Field()
+    """The name of the player."""
     is_starter: Series[bool] = pa.Field()
+    """Whether the player is in the starting lineup."""
     minutes_played: Series[int] = pa.Field()
+    """The number of minutes the player played in the game."""
     jersey_number: Series[int] = pa.Field()
+    """The player's jersey number."""
 
     class Config:  # noqa: D106
         strict = True
@@ -69,12 +89,19 @@ class EventSchema(pa.SchemaModel):
     """Definition of a dataframe containing event stream data of a game."""
 
     game_id: Series[Object] = pa.Field()
+    """The unique identifier for the game."""
     event_id: Series[Object] = pa.Field()
+    """The unique identifier for the event."""
     period_id: Series[int] = pa.Field()
+    """The unique identifier for the part of the game in which the event took place."""
     team_id: Series[Object] = pa.Field(nullable=True)
+    """The unique identifier for the team this event relates to."""
     player_id: Series[Object] = pa.Field(nullable=True)
+    """The unique identifier for the player this event relates to."""
     type_id: Series[int] = pa.Field()
+    """The unique identifier for the type of this event."""
     type_name: Series[str] = pa.Field()
+    """The name of the type of this event."""
 
     class Config:  # noqa: D106
         strict = False
--- socceraction/data/statsbomb/schema.py
+++ socceraction/data/statsbomb/schema.py
@@ -17,25 +17,35 @@ class StatsBombCompetitionSchema(CompetitionSchema):
     """Definition of a dataframe containing a list of competitions and seasons."""
 
     country_name: Series[str]
+    """The name of the country the competition relates to."""
     competition_gender: Series[str]
+    """The gender of the players competing in the competition."""
 
 
 class StatsBombGameSchema(GameSchema):
     """Definition of a dataframe containing a list of games."""
 
     competition_stage: Series[str]
+    """The name of the phase of the competition this game is in."""
     home_score: Series[int]
+    """The final score of the home team."""
     away_score: Series[int]
+    """The final score of the away team."""
     venue: Series[str] = pa.Field(nullable=True)
+    """The name of the stadium where the game was played."""
     referee: Series[str] = pa.Field(nullable=True)
+    """The name of the referee."""
 
 
 class StatsBombPlayerSchema(PlayerSchema):
     """Definition of a dataframe containing the list of players of a game."""
 
     nickname: Series[str] = pa.Field(nullable=True)
+    """The nickname of the player on the team."""
     starting_position_id: Series[int]
+    """The unique identifier for the starting position of the player on the team."""
     starting_position_name: Series[str]
+    """The name of the starting position of the player on the team."""
 
 
 class StatsBombTeamSchema(TeamSchema):
@@ -45,25 +55,46 @@ class StatsBombTeamSchema(TeamSchema):
 class StatsBombEventSchema(EventSchema):
     """Definition of a dataframe containing event stream data of a game."""
 
-    event_id: Series[Object]
     index: Series[int]
+    """Sequence notation for the ordering of events within each match."""
     timestamp: Series[DateTime]
+    """Time in the match the event takes place, recorded to the millisecond."""
     minute: Series[int]
+    """The minutes on the clock at the time of this event."""
     second: Series[int] = pa.Field(ge=0, le=59)
+    """The second part of the timestamp."""
     possession: Series[int]
+    """Indicates the current unique possession in the game."""
     possession_team_id: Series[int]
+    """The ID of the team that started this possession in control of the ball."""
     possession_team_name: Series[str]
+    """The name of the team that started this possession in control of the ball."""
     play_pattern_id: Series[int]
+    """The ID of the play pattern relevant to this event."""
     play_pattern_name: Series[str]
+    """The name of the play pattern relevant to this event."""
     team_name: Series[str]
+    """The name of the team this event relates to."""
     duration: Series[float] = pa.Field(nullable=True)
+    """If relevant, the length in seconds the event lasted."""
     extra: Series[Object]
+    """A JSON string containing type-specific information."""
     related_events: Series[Object]
+    """A comma separated list of the IDs of related events."""
     player_name: Series[str] = pa.Field(nullable=True)
+    """The name of the player this event relates to."""
     position_id: Series[float] = pa.Field(nullable=True)
+    """The ID of the position the player was in at the time of this event."""
     position_name: Series[str] = pa.Field(nullable=True)
+    """The name of the position the player was in at the time of this event."""
     location: Series[Object] = pa.Field(nullable=True)
+    """Array containing the x and y coordinates of the event.
+    """
     under_pressure: Series[bool] = pa.Field(nullable=True)
+    """Whether the action was performed while being pressured by an opponent."""
     counterpress: Series[bool] = pa.Field(nullable=True)
+    """Pressing actions within 5 seconds of an open play turnover."""
     visible_area_360: Optional[Series[Object]] = pa.Field(nullable=True)
+    """An array of coordinates describing the polygon visible to the camera / in the 360 frame."""
     freeze_frame_360: Optional[Series[Object]] = pa.Field(nullable=True)
+    """An array of freeze frame objects."""
