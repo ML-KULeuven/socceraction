36cab47624eb3fd91729644216173e99f59c4cf1
Abhishek Vasu <abhishekvasu@Abhisheks-MacBook-Pro-2.local>
2024-05-08 18:41:44 -0700

    fix(vaep): use iloc to select last value in the dataframe

---
+++ tests/vaep/test_labels.py
@@ -0,0 +1,20 @@
+from pandas import testing as tm
+from pandera.typing import DataFrame
+
+from socceraction.spadl import SPADLSchema
+import socceraction.spadl.utils as spu
+import socceraction.vaep.labels as lab
+
+
+def test_scores(spadl_actions: DataFrame[SPADLSchema]) -> None:
+    nr_actions = 10
+    spadl_actions = spu.add_names(spadl_actions)
+    scores = lab.scores(spadl_actions, nr_actions)
+    assert len(scores) == len(spadl_actions)
+
+
+def test_conceds(spadl_actions: DataFrame[SPADLSchema]) -> None:
+    nr_actions = 10
+    spadl_actions = spu.add_names(spadl_actions)
+    concedes = lab.concedes(spadl_actions, nr_actions)
+    assert len(concedes) == len(spadl_actions)
